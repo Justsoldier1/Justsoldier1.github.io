2dd7bfea720e6e1dd5970aaed51c17e22af3e862
Justsoldier1 <whatTyler@outlook.com>
2026-01-28 19:52:48 +0000

    Made dropdown buttons work using event listeners and not manually calling a function on click

--- Scripts/Dropdown.js
+++ Scripts/Dropdown.js
@@ -1,10 +1,19 @@
-var clicked = false;
+
+function DropdownListeners(){
+    var elements = document.getElementsByClassName("DropdownTitle");
+
+    for(var i = 0; i < elements.length;i++){
+        elements[i].addEventListener("click",ClickDropdown);
+    }
+}
+
 function ClickDropdown(){
-    var element = document.getElementById("DropdownElement");
+    
     if(clicked){
-        element.style.display = "block";
+        this.style.display = "block";
     }else{
-        element.style.display = "none";
+        this.style.display = "none";
     }
     clicked = !clicked;
 }
+
