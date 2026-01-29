4a979d0e37c882c50148ef074af6469e712d6c8e
Justsoldier1 <whatTyler@outlook.com>
2026-01-29 00:59:03 +0000

    Changed e.target.next into a variable and not a function

--- Scripts/Dropdown.js
+++ Scripts/Dropdown.js
@@ -3,7 +3,7 @@ var elements = document.getElementsByClassName("DropdownTitle");
 
 for(i = 0; i < elements.length;i++){
     elements[i].addEventListener('click',function(e){
-        var childobject = e.target.next();
+        var childobject = e.target.next;
         console.log(childobject.innerHTML);
         console.log(childobject.style.display);
         if(childobject.style.display == 'none'){
