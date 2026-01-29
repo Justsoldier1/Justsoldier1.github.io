5ac98fd6990f81a8ee0201dada8ecb3fd98230d4
Justsoldier1 <whatTyler@outlook.com>
2026-01-29 00:32:14 +0000

    Replaced this with e

--- Scripts/Dropdown.js
+++ Scripts/Dropdown.js
@@ -3,15 +3,16 @@ var elements = document.getElementsByClassName("DropdownTitle");
 
 window.addEventListener('load',function(){
     for(i = 0; i < elements.length;i++){
-        elements[i].addEventListener('click',ClickDropdown);
+        elements[i].addEventListener('click',ClickDropdown (e));
     }
 });
 
 var clicked = false;
 
-function ClickDropdown(){
-    var childobject = this.nextElementSibling;
-    console.log(childobject.InnerHTML);
+function ClickDropdown(e){
+    
+    var childobject = e.nextElementSibling;
+    console.log(childobject.innerHTML);
     console.log(childobject.style.display);
     if(childobject.style.display == 'none'){
         childobject.style.display = "display";
