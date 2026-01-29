e8a24acf2baaed8533bbb3a7ff451489a78fb6df
Justsoldier1 <whatTyler@outlook.com>
2026-01-29 00:55:42 +0000

    Made e work

--- Scripts/Dropdown.js
+++ Scripts/Dropdown.js
@@ -1,17 +1,23 @@
 
 var elements = document.getElementsByClassName("DropdownTitle");
 
-window.addEventListener('load',function(e){
-    for(i = 0; i < elements.length;i++){
-        elements[i].addEventListener('click',ClickDropdown (e));
-    }
-});
+for(i = 0; i < elements.length;i++){
+    elements[i].addEventListener('click',function(e){
+        var childobject = e.target.next();
+        console.log(childobject.innerHTML);
+        console.log(childobject.style.display);
+        if(childobject.style.display == 'none'){
+            childobject.style.display = "display";
+        }else{
+            childobject.style.display = "none";
+        }
+    });
+}
 
 var clicked = false;
 
-function ClickDropdown(e){
+function ClickDropdown(){
     
-    var childobject = e.nextElementSibling;
     console.log(childobject.innerHTML);
     console.log(childobject.style.display);
     if(childobject.style.display == 'none'){
